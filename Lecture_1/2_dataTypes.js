72f013a2a2fd6a482ec09ab3bc3a268ccf4fa5b2
shyam <ayushmbd21102002@gmail.com>
2025-12-15 21:32:17 +0530

    Data Type

---
+++ Lecture_1/2_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser   
+
+// code readability should be high
+
+// Data Type
+
+let name = "Ayush" // string
+let age = 24 // number
+let isLoggedIn = true // boolean
+
+// number = 2 to power 53
+// bigint = big number
+// string = ""
+// boolean = true/false
+// null = standalone value data type -> let a = null -> a contains null datatype , null = empty
+// undefined -> let state; -> no value is assigned
+// symbol = unique -> help to find unique 
+
+// object
+
+console.log(typeof "Ayush"); // string
+console.log(typeof age); // number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object -> null type is object
+
+
+
